3c4b958063b8e07c7a9c9c6aef7124ff0545cea3
muhamd sohaib <msohaib980@gmail.com>
2017-07-23 23:41:59 -0400

    working on vido list

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
-import SearchBar from './components/search_bar'
-
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = "AIzaSyBzUSqr_4KUMXyvnvVWe9VHh7QVtMqPhgk";
 
-const App = function(){
-  return <div>
-    <SearchBar />
-  </div>;
+
+class App extends Component{
+
+  constructor(props){
+    super(props)
+    this.state = {videos:[]};
+
+    YTSearch({key:API_KEY,term:'surfboad'},(videos) => {
+      this.setState({videos});
+
+    })
+  }
+
+  render(){
+    return (
+    <div>
+      <SearchBar />
+      <VideoList videos={this.state.videos} />
+
+    </div>
+    );
+
+  }
 
 }
 
